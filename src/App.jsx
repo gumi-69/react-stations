bf21b65a89a5b032130d1c2614d88bde575beda3
gumi-69 <irohaloid10963@gmail.com>
2024-06-26 11:13:49 +0900

    jsxに説明と画像を追加

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import './App.css'
  */
 export const App = () => {
   return (
-    <header>Dogアプリ</header>
+    <header>
+      <h1>Dogアプリ</h1>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
+    </header>
   )
 }
