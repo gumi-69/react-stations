cbc5ce18cd04e0bfe79b26698e3d0feab99a7b36
gumi-69 <irohaloid10963@gmail.com>
2024-08-01 22:08:25 +0900

    setDogUrlにAPIからの値を入れた

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
+
+
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg")
+  const fetchRandomDogImage = async () => {
+    try{
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const data = await response.json();
+      setDogUrl(data.message);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  }
+
   return (
     <header>
       <h1>Dogアプリ</h1>
       <p>犬の画像を表示するサイトです。</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" />
+      <img src={dogUrl} />
+      <button onClick={fetchRandomDogImage}>画像を変更</button>
     </header>
   )
 }
